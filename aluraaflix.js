e102a632f71f8819e2e35815c79fab7ab1accf70
Gustavo <66639892+gcostacoelho@users.noreply.github.com>
2021-04-13 11:25:04 -0300

    Add files via upload

---
+++ aluraaflix.js
@@ -0,0 +1,27 @@
+function movie1(){
+    var result = document.getElementById("result")
+    result.innerHTML=("<h2>Flashpoint</h2>")
+    result.innerHTML+=('<img src=' + "https://m.media-amazon.com/images/M/MV5BMTgwNTljYzgtOTU3ZC00ZjhhLTk0YzItY2RiMWU0MGZlNzFjL2ltYWdlXkEyXkFqcGdeQXVyNDQ2MTMzODA@._V1_UY268_CR2,0,182,268_AL_.jpg" + '>')
+    result.innerHTML+=('<h3>More informations <a href="https://www.imdb.com/title/tt2820466/" target="_blank">here</a></h3>')
+  }
+  
+  function movie2(){
+    var result = document.getElementById("result")
+    result.innerHTML=("<h2>Justice League</h2>")
+    result.innerHTML+=('<img src=' + "https://m.media-amazon.com/images/M/MV5BYjI3NDg0ZTEtMDEwYS00YWMyLThjYjktMTNlM2NmYjc1OGRiXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_UX182_CR0,0,182,268_AL_.jpg" + '>')
+    result.innerHTML+=('<h3>More informations <a href="https://www.imdb.com/title/tt12361974/?ref_=nv_sr_srsg_0" target="_blank">here</a></h3>')
+  }
+  
+  function movie3(){
+    var result = document.getElementById("result")
+    result.innerHTML=("<h2>The Dark Knight</h2>")
+    result.innerHTML+=('<img src=' + "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg" + '>')
+    result.innerHTML+=('<h3>More informations <a href="https://www.imdb.com/title/tt0468569?ref_=nv_sr_srsg_0" target="_blank">here</a></h3>')
+  }
+  
+  function movie4(){
+    var result = document.getElementById("result")
+    result.innerHTML=("<h2>Superman</h2>")
+    result.innerHTML+=('<img src=' + "https://m.media-amazon.com/images/M/MV5BNzY2ZDQ2MTctYzlhOC00MWJhLTgxMmItMDgzNDQwMDdhOWI2XkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX182_CR0,0,182,268_AL_.jpg" + '>')
+    result.innerHTML+=('<h3>More informations <a href="https://www.imdb.com/title/tt0348150?ref_=nv_sr_srsg_7" target="_blank">here</a></h3>')
+  }
